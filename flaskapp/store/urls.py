e2fa85b4d000dab0d9bedc94e401d2a6490978d0
Raul <rrguardo83@gmail.com>
2016-01-11 02:04:29 -0400

    Updates

--- flaskapp/store/urls.py
+++ flaskapp/store/urls.py
@@ -11,6 +11,7 @@ from flaskapp.store import store_bp
 from flaskapp.store.views import *
 
 
-store_bp.add_url_rule('/paypal_ipn', 'paypal_ipn', paypal_ipn)
+store_bp.add_url_rule('/paypal_ipn', 'paypal_ipn', paypal_ipn, methods=['POST',
+    'GET'])
 store_bp.add_url_rule('/success.html', 'success', success)
 store_bp.add_url_rule('/cancel.html', 'cancel', cancel)
